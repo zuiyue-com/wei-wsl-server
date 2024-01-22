4b87e929815e1595620b8c851b80b22e810971fe
HuangWei <market@zuiyue.com>
2024-01-22 17:53:32 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+#[tokio::main(flavor = "multi_thread", worker_threads = 20)]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    wei_windows::init();
+    wei_env::bin_init("wei");
+    
+    Ok(())
+}
